c2f218ced06107cc6517e82a948b1b9b85bc22ad
saajan.varghese.2006@gmail.com <saajan.varghese.2006@gmail.com>
2025-12-14 02:55:43 +0530

    twilio messaging service done for alerting users

--- backend/notification_agent.py
+++ backend/notification_agent.py
@@ -1,11 +1,25 @@
 import json
 import time
+from twilio.rest import Client
 from datetime import datetime
 from utils import get_mongo_client,get_redis_client,Config
 
-def send_whatsapp(phone: str,message:str):
-    print("Whatsapp sent to phone{phone}:{message}")
-    return "sent"
+twilio_client=Client(
+    Config.twilio_account_sid,
+    Config.twilio_auth_token
+)
+whatsapp_from=Config.twilio_from_number
+
+def send_whatsapp(to_number: str,message:str):
+    try:
+        msg=twilio_client.messages.create(
+            from_=whatsapp_from,
+            to=f"whatsapp:{to_number}",
+            body=message
+        )
+        return "sent"
+    except Exception as e:
+        return f"FAILED: {str(e)}"
 
 def send_voice_call(phone: str,message:str):
     print("Voice call placed to {phone}:{message}")
@@ -30,11 +44,21 @@ def start_notification_worker():
                 severity = alert["severity"]
                 alert_type = alert["alert_type"]
                 value = alert["value"]
-            
-                message=(
-                    f"Alert:{alert_type} detected for vehicle {vehicle_id}"
-                    f"Value:{value} Severity:{severity}"
+
+                customer_phone=+919444082981
+
+                message = (
+                    f"Vehicle Alert Detected\n\n"
+                    f"Vehicle ID: {vehicle_id}\n"
+                    f"Issue: {alert_type}\n"
+                    f"Value: {value}\n"
+                    f"Severity: {severity}\n\n"
+                    f"Our system recommends service. "
+                    f"Reply YES to proceed."
                 )
+
+                status=send_whatsapp(customer_phone,message)
+
                 if severity == "HIGH":
                     status = send_voice_call(vehicle_id, message)
                     channel = "VOICE"
@@ -69,7 +93,7 @@ def start_notification_worker():
                 "timestamp": datetime.utcnow()
             })
 
-        time.sleep(3)
+        time.sleep(5)
 
 if __name__=="__main__":
     start_notification_worker()
